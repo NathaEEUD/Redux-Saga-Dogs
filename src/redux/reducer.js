81bda3b0c2677624ecff2f722e439adf5d61607f
Nathaly <natharb09@gmail.com>
2019-10-11 19:38:22 -0500

    feat: adding actions, reducers and sagas of redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+import { API_CALL_FAILURE, API_CALL_REQUEST, API_CALL_SUCCESS } from './actions';
+
+const INIT_STATE = {
+  fetching: false,
+  dog: null,
+  error: null
+};
+
+export function reducer(state = INIT_STATE, action) {
+  switch (action.type) {
+    case API_CALL_REQUEST:
+      return { ...state, fetching: true, error: null };
+
+    case API_CALL_SUCCESS:
+      return { ...state, fetching: false, dog: action.dog };
+
+    case API_CALL_FAILURE:
+      return { ...state, fetching: false, dog: null, error: action.error };
+
+    default:
+      return state;
+  }
+}
